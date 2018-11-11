3394b54d9132cfd72a7530810578403c6b03ba76
Devin Andres Salemi <devinsalem@gmail.com>
2018-11-10 19:29:26 -0800

    Add files via upload

--- src/app/models/application.model.ts
+++ src/app/models/application.model.ts
@@ -1,1 +1,15 @@
- 
+ export class Applicant {
+     id: number;
+     userId: number;
+     resumeId: number;
+     salary: number;
+     firstName: string;
+     lastName: string;
+     gender: string;
+     dateApplied: Date;
+     job: string;
+     address: string;
+     city: string;
+     state: string;
+     zip: number;
+ }
